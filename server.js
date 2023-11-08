5ba7d45b70f8df23cfdd9452b0d0e446b7fd66c0
pauloh-fm <phfarmar@gmail.com>
2023-11-07 22:32:24 -0300

    [feat] refatoração do server

--- server.js
+++ server.js
@@ -1,61 +1,22 @@
-const express = require('express');
-const { Client } = require('pg');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+// server.js
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = require('./src/app');
+const mongoose = require('mongoose');
 
-app.use(cors());
-app.use(bodyParser.json());
+const port = process.env.PORT || 3002;
+// TODO: Conexão com BD
+// const dbURI = 'mongodb://localhost/avaliacao-atendimento'; // Substitua pelo seu URI de banco de dados
 
-// Configuração para a conexão ao banco de dados PostgreSQL
-const db = new Client({
-  user: 'root',
-  host: 'localhost',
-  database: 'test_db',
-  password: 'root',
-  port: 5432,
-});
+// mongoose.connect(dbURI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-db.connect()
-  .then(() => {
-    console.log('Conexão ao banco de dados PostgreSQL estabelecida.');
-  })
-  .catch((err) => {
-    console.error('Erro na conexão ao banco de dados:', err);
-  });
-
-// Rota para criar uma nova avaliação
-app.post('/avaliacoes', (req, res) => {
-  const { classificacao, comentario } = req.body;
-  const query = 'INSERT INTO avaliacoes (classificacao, comentario) VALUES ($1, $2) RETURNING *';
-
-  db.query(query, [classificacao, comentario])
-    .then((result) => {
-      res.status(201).json(result.rows[0]);
-    })
-    .catch((err) => {
-      console.error('Erro ao inserir avaliação:', err);
-      res.status(400).send(err);
-    });
-});
-
-// Rota para listar todas as avaliações
-app.get('/avaliacoes', (req, res) => {
-  const query = 'SELECT * FROM avaliacoes';
-
-  db.query(query)
-    .then((result) => {
-      res.status(200).json(result.rows);
-    })
-    .catch((err) => {
-      console.error('Erro ao buscar avaliações:', err);
-      res.status(500).send(err);
-    });
-});
-
-// Outras rotas para atualização, exclusão, etc.
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'Erro na conexão ao banco de dados:'));
+// db.once('open', () => {
+//   console.log('Conexão ao banco de dados estabelecida.');
+// });
 
 app.listen(port, () => {
   console.log(`Servidor Express rodando na porta ${port}`);
