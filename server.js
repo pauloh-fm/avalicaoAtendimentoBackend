35cc64c33bac38c7640d014f1367605a84721bd7
pauloh-fm <phfarmar@gmail.com>
2023-12-01 01:28:22 -0300

    [feat] update server js

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
 // server.js
 
-const app = require('./src/app');
-const mongoose = require('mongoose');
+import app from './src/app.js'; 
+// const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3002;
-// TODO: Conexão com BD
-// const dbURI = 'mongodb://localhost/avaliacao-atendimento'; // Substitua pelo seu URI de banco de dados
-
-// mongoose.connect(dbURI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'Erro na conexão ao banco de dados:'));
-// db.once('open', () => {
-//   console.log('Conexão ao banco de dados estabelecida.');
-// });
 
 app.listen(port, () => {
   console.log(`Servidor Express rodando na porta ${port}`);
